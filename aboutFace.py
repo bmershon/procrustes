e3bc5a32b90dfd0d334b3a2f8a5a80828624fa7f
Brooks Mershon <mershon.brooks@gmail.com>
2016-03-13 14:56:58 -0400

    rotate mesh 180

--- aboutFace.py
+++ aboutFace.py
@@ -1,21 +1,23 @@
-#Programmer: Chris Tralie
-#Purpose: To provide a sample script for performing manipulates of
-#off file geometry
+# Adapted from Chris Tralie's manipulateGeometry.py
 import sys
 sys.path.append("S3DGLPy")
 from PolyMesh import *
 import numpy as np
 
 if __name__ == '__main__':
-    meshin = "meshes/StyrofoamHead.off"
-    meshout = "meshes/StyrofoamHead-rotated.off"
+    if len(sys.argv) < 2:
+        sys.exit(1)
+    meshin = sys.argv[1]
+    meshout = sys.argv[2]
     m = PolyMesh()
     (VPos, VColors, ITris) = loadOffFileExternal(meshin)
     #Note: Points come into the program as a matrix in rows, so we need
     #to transpose to get them into our column format
     VPos = VPos.T
     #Make a random rotation matrix
-    [R, _, _] = np.linalg.svd(np.random.randn(3, 3))
+    R = np.eye(3, 3)
+    R[2,2] = -1
+    print(R)
     #Apply rotation
     VPos = R.dot(VPos)
     #Save the results
